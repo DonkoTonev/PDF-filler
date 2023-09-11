fc34d66df6d0d36eef6d29e4baf6569695396efc
DonkoTonev <116662870+DonkoTonev@users.noreply.github.com>
2023-09-11 11:45:21 +0300

    Update filler.py
    Comments added

--- filler.py
+++ filler.py
@@ -1,38 +1,49 @@
+# Import necessary libraries
 import PyPDF2
 import json
 
-# Път до вашия PDF файл
+# Define the file paths for the input PDF and JSON data
 pdf_file_path = "example.pdf"
-
-# Път до вашия JSON файл със стойности за попълване
 json_file_path = "file.json"
 
-# Отваряне на JSON файл и зареждане на стойностите в речник
+# Open the JSON file for reading
 with open(json_file_path, 'r') as json_file:
+    # Load the JSON data into a Python dictionary
     data = json.load(json_file)
 
-# Отваряне на PDF файла
+# Use PyPDF2 to open the PDF file
 pdf = PyPDF2.PdfReader(pdf_file_path)
 
-# Проверка за наличие на попълними полета в PDF файла
+fields = pdf.get_fields()
+
+# Check if the PDF is encrypted
 if pdf.is_encrypted:
-    print("PDF файлът е кодиран и не може да бъде попълнен.")
+    print("PDF is encrypted")
 else:
+    # Create a PDF writer object to create a new PDF with filled form fields
     pdf_writer = PyPDF2.PdfWriter()
 
-    # Обхождане на всички страници на PDF файла
+    # Iterate through each page in the input PDF
     for page_num in range(len(pdf.pages)):
+        # Get the current page
         page = pdf.pages[page_num]
 
-        # Попълване на полетата в текущата страница със стойности от JSON
+        # Iterate through the fields and values in the JSON data
         for field_name, field_value in data.items():
+            # Update the form field values on the current page
             pdf_writer.update_page_form_field_values(page, {field_name: field_value})
 
+        # Add the updated page to the PDF writer
         pdf_writer.add_page(page)
 
-    # Запис на резултата в нов PDF файл
-    output_pdf_path = "C:\\Users\\admin\\OneDrive\\Desktop\\DEMO\\sample.pdf"
+    # Specify the output PDF file path
+    output_pdf_path = "C:\\Users\\admin\\OneDrive\\Desktop\\sample.pdf"
+
+    # Open the output PDF file for writing in binary mode
     with open(output_pdf_path, 'wb') as output_pdf_file:
+        # Write the PDF content to the output file
         pdf_writer.write(output_pdf_file)
 
-    print(f"PDF файла беше успешно попълнен и записан в {output_pdf_path}")
+    # Print a success message with the path to the filled PDF
+    print(f"PDF was successfully filled at {output_pdf_path}")
+
